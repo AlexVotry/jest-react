04c654c91e3de3af6a3b8d31b9b00d3e3e3ca223
Alex Votry <youremail@gmail.com>
2020-05-16 10:00:51 -0700

    set up for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <div>
       <h1>Jest Test with React</h1>
