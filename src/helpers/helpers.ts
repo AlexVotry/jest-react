1dcf912b4d18fe22a2d9943d38e4e0a96bc47bb8
Alex Votry <youremail@gmail.com>
2020-05-29 11:52:03 -0700

    ;

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -3,4 +3,4 @@ export function getLetterMatchCount(guessedWord: string, secretWord: string) {
   const guessedLetterSet = new Set(guessedWord.split(''));
 
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
-}
+};
